abe92d041c65895f93be36edd0a5a32f5dd50a35
Andreas Nilsson <and.nil@outlook.com>
2017-11-26 22:51:17 +0100

    asdf

--- index.js
+++ index.js
@@ -1,82 +1,13 @@
-const repo = require('./config/mongo');
-const express = require('express')
-const session = require('express-session')
-const bodyParser = require('body-parser')
-const cookieParser = require('cookie-parser');
-const path = require('path')
-const passport = require('passport')
-const https = require('https')
-const requireLogin = require('./middlewares/requireLogin');
-const fs = require('fs')
-var util = require('util');
-require('./config/passport');
+var http = require('http');
 
-const app = express()
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+var server = http.createServer(function(request, response) {
 
-app.use(cookieParser());
-app.use(session({
-    secret: 'wowapp',
-    saveUninitialized: true,
-    resave: true
-}));
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("vafan");
 
-
-app.use(passport.initialize());
-app.use(passport.session());
-
-app.use(express.static(path.join(__dirname, 'client/')));
-
-console.log(`bnet id:${process.env.BNET_ID}`);
-console.log(`bnet sercret:${process.env.BNET_SECRET}`);
-console.log(`auth callback:${process.env.AUTHCALLBACK}`);
-console.log(`port:${process.env.PORT}`);
-
-app.get('/auth/bnet', passport.authenticate('bnet'));
-
-app.get('/auth/bnet/callback',
-    passport.authenticate('bnet', { failureRedirect: '/error' }),
-    function (req, res) {
-        res.redirect('/');
-    });
-
-app.get('/api/user', requireLogin, async (req, res) => {
-    console.log(req.user ? req.user : 'ingen user finns, detta borde inte synas?');
-    res.send(req.user);
-})
-
-app.get('/api/players', async (req, res) => {
-    var players = await repo.getAllPlayers();
-    res.send(players);
 });
 
-app.get('/api/player', requireLogin, async (req, res) => {
-    if (!req.user) throw "no user found";
-    var player = await repo.getPlayer(req.user.id);
-    res.send(player);
-});
+var port = process.env.PORT || 1337;
+server.listen(port);
 
-app.post('/api/player', requireLogin, async (req, res) => {
-    if (!req.user) throw "no user found";
-    if (!req.body) throw "no body"
-
-    req.body.ownerid = req.user.id;
-    req.body.ownername = req.user.battletag;
-    await repo.saveplayer(req.body);
-    res.send();
-});
-
-app.get('/logout', function (req, res) {
-    req.logout();
-    res.redirect('/');
-});
-
-app.get('*', function (req, res) {
-    res.sendFile(path.resolve('client/index.html'));
-});
-
-app.listen(process.env.PORT);
-
-
-
+console.log("Server running at http://localhost:%d", port);
